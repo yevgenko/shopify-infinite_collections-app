207d904efda49153f84a87f9548c76a10c5c27bf
Yevgeniy A. Viktorov <craftsman@yevgenko.me>
2013-03-26 13:59:43 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopifyIcollection::Application.config.secret_token = 'fe2a6d5e244d223d921431025af23cb31b0f6cfa65707c65cc3bbf143d3430db4c825ac7d29b881ac7f843c1456932eb34da7e56a66e2189d232ff3b62b01d62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ShopifyIcollection::Application.config.session_store :cookie_store, key: '_shopify-iCollection_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ShopifyIcollection::Application.config.session_store :active_record_store
